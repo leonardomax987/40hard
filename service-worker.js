be5e76befc62dc94c2ef3dfbe1061a0a8d91f3f5
leonardomax987 <leonardomax9871211@gmail.com>
2026-07-04 15:48:28 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,19 @@
-const CACHE = '40hard-v1';
+const CACHE = '40hard-v2';
 const FILES = ['/40hard/', '/40hard/index.html', '/40hard/manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
